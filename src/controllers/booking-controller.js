1dac99eac037d409667ebef0f52d947b6e4b35d9
Shravya270 <shravyashetty246@gmail.com>
2025-11-06 15:51:12 +0530

    done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,8 +1,9 @@
 const {BookingService} = require('../services');
 const { SuccessResponse,ErrorResponse } = require('../utils/common');
 const {statusCode, StatusCodes} = require('http-status-codes');
+const { message } = require('../utils/common/error-response');
 
-
+const inMemDb=[];
 
 async function createBooking(req,res){
     try{
@@ -23,11 +24,20 @@ async function createBooking(req,res){
 
 async function makePayment(req,res){
     try{
+        const idempotencyKey = req.headers['x-idempotency-key'];
+        if(!idempotencyKey){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:'Idempotency Key missing'});
+        }
+        if(inMemDb[idempotencyKey]){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:'Cannot retry on a successful payment'});
+        }
         const response = await BookingService.makePayment({
             totalCost:req.body.totalCost,
             userId:req.body.userId,
             bookingId:req.body.bookingId,
         });
+        inMemDb[idempotencyKey]=idempotencyKey;
+
         SuccessResponse.data = response;
         return res.status(StatusCodes.OK).json(SuccessResponse);
     }
